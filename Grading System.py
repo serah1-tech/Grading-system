39d0ce58ae17bed34cfdb4150e45a7e0bf891216
serah1-tech <78899267+serah1-tech@users.noreply.github.com>
2024-05-25 09:54:36 +0300

    Add files via upload

---
+++ Grading System.py
@@ -0,0 +1,11 @@
+grade= int(input("Enter you grade: "))
+if grade>= 70:
+    print("A")
+elif grade >=60:
+    print ("B")
+elif grade >= 50:
+    print("C")
+elif grade >= 40:
+    print("D")
+else:
+    print ("You failed")
